36187fdbd368203cd9d41ffda7f0148721b837b6
bakai0903 <bakai0903@gmail.com>
2025-06-30 16:24:44 +0600

    Added Contact model and migrate

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -1,7 +1,24 @@
+import uuid
+
 from django_tenants.models import TenantMixin
 from django.db import models
+
 
 class Tenant(TenantMixin):
     name = models.CharField(max_length=100)
 
     auto_create_schema = True
+
+    class Meta:
+        tenant_model = False
+
+
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=50, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
