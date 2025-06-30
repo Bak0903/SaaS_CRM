2f31c00feaabe4cd4748d84c39f7942d86ffb529
bakai0903 <bakai0903@gmail.com>
2025-06-30 16:24:15 +0600

    Added CLI-command

---
+++ src/contacts/management/commands/create_schema.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from contacts.models import Tenant
+
+
+class Command(BaseCommand):
+    help = "Create new tenant schema"
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument("schema", type=str, help="Schema name (e.g. acme)")
+        parser.add_argument("name", type=str, help="Tenant name")
+
+    def handle(self, *args, **options):
+        schema = f"contact_{options['schema']}"
+        name = options["name"]
+
+        if Tenant.objects.filter(schema_name=schema).exists():
+            self.stdout.write(self.style.WARNING("Tenant already exists"))
+            return
+
+        tenant = Tenant(schema_name=schema, name=name)
+        tenant.save()
+        self.stdout.write(self.style.SUCCESS(f"Tenant created: {name} ({schema})"))
