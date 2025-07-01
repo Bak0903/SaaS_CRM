9c6b10656b69f0b404a49d6d85d06eefba3513c5
bakai0903 <bakai0903@gmail.com>
2025-07-01 13:04:04 +0600

    Added celery settings to settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -150,3 +150,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
